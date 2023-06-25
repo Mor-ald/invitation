86164c1b545ee2a5910a0666965aa6b9947b1068
Mor-ald <mor.ald@yandex.ru>
2023-06-26 00:25:58 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Time from "./components/time/Time";
 import Dress from "./components/dress/Dress";
 import Remained from "./components/remained/Remained";
 import Wishes from "./components/wishes/Wishes";
-import Questionnaire from "./components/form/Questionnaire";
 import Contacts from "./components/contacts/Contacts";
 
 function App() {
